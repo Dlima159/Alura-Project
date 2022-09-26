57fb68f601eaca7737007e0d74994276f51df06f
Deivison Lima <deivisonjob159@gmail.com>
2022-09-25 21:15:06 -0300

    Calculadora de Média

---
+++ Calculadora de Média/aula1.js
@@ -0,0 +1,11 @@
+var nome = "Deivison"
+var NotaDoPrimeiroBimestre = 9
+var NotaDoSegundoBimestre = 8.5
+var NotaDoTerceiroBimestre = 7.5
+var NotaDoQuartoBimestre = 7
+
+var NotaFinal = (NotaDoPrimeiroBimestre+NotaDoSegundoBimestre+NotaDoTerceiroBimestre+NotaDoQuartoBimestre)/4
+
+console.log("Bem vindo "+nome)
+
+console.log("Sua média é "+NotaFinal).toFixed(1)
